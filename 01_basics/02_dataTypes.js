9d56e94309a956597ae41827c5e10505147d9be0
Raj Gahoi <gahoiraj01@gmail.com>
2026-06-07 12:42:55 +0000

    data types lesson

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3)// we are using nodejs,not browser
+console.log(3
+    +
+    3) //code readibility matters
+    console.log("Raj")
+    let name = "Raj"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+    //number => 2 to pow 53
+    //bigint
+    //string => "
+    //boolean =>t/f
+    //null =>standalone value
+    //symbol => unique
+    //object
+    console.log(typesof undefined);//undefined
+    console.log(typeof null);//object
+
+//semicolon does not matter 
